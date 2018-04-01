2b793057ed69d67abeab3af940ebe77751c7c782
ettorebevilacqua <ettorebevilacqua@gmail.com>
2018-04-01 10:30:56 +0200

    prova commit

--- store/index.js
+++ store/index.js
@@ -1,32 +1,36 @@
 import Axios from 'axios'
 
-
-
 export const state = () => ({
- payments: [],
-    balance: {'prov':{'amount':'0','prov':'0'},'cashBack':{'amount':'0','cashback':'0'},'balance':0}
+  payments: [],
+  balance: {'prov': {'amount': '0', 'prov': '0'}, 'cashBack': {'amount': '0', 'cashback': '0'}, 'balance': 0}
 })
 
 export const actions = {
-     getPayment(state) {
-        const {data} =  Axios.get('http://www.19thstreet.it/naike/a/shop/api.php?api=payment').then(res=>{
-            state.commit('setPayments', res.data)
-        })
-    },
-    getBalance(state) {
-       const {data} =  Axios.get('http://www.19thstreet.it/naike/a/shop/api.php?api=balance').then(res=>{
-           state.commit('setBalance', res.data)
-       })
-   }
+  getPayment (state) {
+    return Axios.get('http://www.19thstreet.it/naike/a/shop/api.php?api=payment').then(res => {
+      state.commit('setPayments', res.data)
+    })
+  },
+  getBalance (state) {
+    return Axios.get('http://www.19thstreet.it/naike/a/shop/api.php?api=balance').then(res => {
+      state.commit('setBalance', res.data)
+    })
+  },
+
+  getUser (state) {
+    return Axios.get('http://www.19thstreet.it/naike/a/shop/api.php?api=balance').then(res => {
+      state.commit('setBalance', res.data)
+    })
+  }
 
 }
 
 export const mutations = {
-    setPayments: (state, data) => {
-        state.payments = data // Object.assign({}, data)
-    },
-    setBalance: (state, data) => {
-        state.balance = data
-    }
+  setPayments: (state, data) => {
+    state.payments = data // Object.assign({}, data)
+  },
+  setBalance: (state, data) => {
+    state.balance = data
+  }
 
 }
